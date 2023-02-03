94fb601c206749f6d63ecf75ff8486c94cb7cbab
Genyus <genyus@gmail.com>
2023-02-03 04:44:44 +0000

    chore: optimised performance Simplified to a single iteration across input string

--- swift/advent-of-code-2022-day1.playground/Contents.swift
+++ swift/advent-of-code-2022-day1.playground/Contents.swift
@@ -2,19 +2,22 @@ import UIKit
 
 func calorieCalculator(with calories: String) -> Int {
     // Split the multi-line string into a 2D array, separated by line breaks
-    var max = 0
-    let elfArray = calories.split(separator: "\n\n")
+    var maxCalories = 0
+    var elfCalories = 0
     
-    // Execute a reduce on each sub-array, tracking the maximum as we go
-    for elf in elfArray {
-        let elfTotal = elf.split(separator: "\n").reduce(0) { $0 + Int($1)! }
-        
-        if (elfTotal > max) {
-            max = elfTotal
+    calories.enumerateLines { (line, _) in
+        if(line.isEmpty) {
+            if(elfCalories > maxCalories) {
+                maxCalories = elfCalories
+            }
+            elfCalories = 0
+        }
+        else {
+            elfCalories += Int(line)!
         }
     }
     
-    return max
+    return maxCalories
 }
 
 let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
